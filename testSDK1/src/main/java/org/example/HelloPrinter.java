4b8bdda8f0a9decddd4a3a5180a45906b5ca6734
Ahmed Hamdy <ahmed.hamdyfahmy@vodafone.com>
2025-12-10 15:58:20 +0200

    Update HelloPrinter.java

--- testSDK1/src/main/java/org/example/HelloPrinter.java
+++ testSDK1/src/main/java/org/example/HelloPrinter.java
@@ -4,7 +4,7 @@ public class HelloPrinter {
 
     // Public function to print "Hello World 1"
     public static void printHelloWorld() {
-        System.out.println("Hello World 1");
+        System.out.println("Hello World-1");
     }
 
 }
